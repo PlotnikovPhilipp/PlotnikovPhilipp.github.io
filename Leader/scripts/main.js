bcca571594ac415202b9495e6010add6ef1260df
PlotnikovPhilipp <fi07101953@gmail.com>
2019-09-22 12:22:08 +0500

    --allow-empty-message

---
+++ Leader/scripts/main.js
@@ -0,0 +1,9 @@
+bar.style.height = (window.pageYOffset + window.outerHeight) / document.documentElement.offsetHeight * bar.parentNode.offsetHeight + "px";
+function chnageBar() {
+    bar.style.height = (window.pageYOffset + window.outerHeight) / document.documentElement.offsetHeight * bar.parentNode.offsetHeight + "px";
+}
+if (window.addEventListener) {
+    window.addEventListener('scroll', chnageBar, false);
+} else {
+    window.RTCDataChannelEvent('onscroll', chnageBar);
+}
