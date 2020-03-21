6d435e6d95e3135932d03f1f788ca8326bd63c55
PlotnikovPhilipp <fi07101953@gmail.com>
2020-03-21 13:09:49 +0500

    Philipp

---
+++ ProgrammForMetaTags/clientScripts/main.js
@@ -0,0 +1,54 @@
+//Change the meta tag, that specified in the form
+function changeMetaTag(event) {
+    event.preventDefault();
+    let selfUrl = siteUrl.value; 
+    let selfRequest = request.value;
+
+    let headers = selfRequest.match(/\[\w*\]/g);
+    let values = selfRequest.match(/\{\w*\}/g);
+
+    //Delete the first and last symbol in headers
+    headers.forEach(element => {
+        return element.substr(1, element.length-1);
+    });
+
+    //Delete the first and last symbol in values
+    values.forEach(element => {
+        return element.substr(1, element.length-1);
+    });
+
+    //Request the server for site   
+    fetch(siteUrl).then((element) => {
+        return element.text();
+    }).then((siteCode) => {
+
+    //Find the meta tag and change it
+    console.log(siteCode);
+    });
+}
+
+//Gather info before draw content on screen
+function initiate() {
+    let button = document.getElementsByTagName('button')[0];
+    siteUrl = document.getElementsByName('url')[0];
+    request = document.getElementsByName('metaName')[0];
+
+    //Install event handler for submit button
+    if(flag) {
+        button.addEventListener('click', changeMetaTag, false);
+    } else {
+        button.attachEvent('onclick', changeMetaTag);
+    }
+}
+
+//Install event handler for loaded DOM
+let flag = null;
+let siteUrl = null;
+let request = null;
+if(document.addEventListener) {
+    flag = true;
+    document.addEventListener('DOMContentLoaded', initiate, false)
+} else {
+    flag = false;
+    document.attachEvent('onDOMContentLoaded', initiate);
+}
