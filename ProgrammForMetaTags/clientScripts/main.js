9fc05fbc8e3f6a0cb26c8db255bf175b67cfe318
PlotnikovPhilipp <fi07101953@gmail.com>
2020-03-21 15:59:37 +0500

    Update main.js

--- ProgrammForMetaTags/clientScripts/main.js
+++ ProgrammForMetaTags/clientScripts/main.js
@@ -25,11 +25,11 @@ function changeMetaTag(event) {
     //Find the meta tag and change it
     headers.forEach((element, index) => {
         let pattern = new RegExp('<meta name="' + element + '" >');
-        let copyOfsiteCode = siteCode.value;
-        siteCode.replace(pattern, '<meta name="' + element + '" content="' + values[index] + '"');
+        let copyOfsiteCode = siteCode.result;
+        copyOfsiteCode.replace(pattern, '<meta name="' + element + '" content="' + values[index] + '"');
+        console.log(copyOfsiteCode);
     });
     });
-    console.log
 }
 
 //Gather info before draw content on screen
