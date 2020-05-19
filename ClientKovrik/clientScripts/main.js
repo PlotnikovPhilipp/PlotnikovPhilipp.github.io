3479bb7cadf05b373fa16598a5c8a44d5d48616f
PlotnikovPhilipp <43723175+PlotnikovPhilipp@users.noreply.github.com>
2020-05-19 13:19:53 +0500

    Update main.js

--- ClientKovrik/clientScripts/main.js
+++ ClientKovrik/clientScripts/main.js
@@ -760,25 +760,20 @@ function addToBin() {
     // Add complects with choden options
     let openWindow = null;
     let complectCard = null;
-    console.log(goodOptions.complect);
-    console.log(complectOptions.complect == 'Водительскй');
     switch(goodOptions.complect) {
         case 'Водительский':
-            сonsole.log('Водительский');
             complectCard = document.getElementsByClassName('js-store-prod-btn t-store__card__btn t-btn t-btn_sm')[DRIVER];
             complectCard.dispatchEvent(new Event('click'));
             document.body.style.overflow = 'scroll';
             setTimeout(initComplectOptions, 50, goodOptions);
             break;
         case 'Водительский и пассажирский':
-            сonsole.log('Водительский и пассажирский');
             complectCard = document.getElementsByClassName('js-store-prod-btn t-store__card__btn t-btn t-btn_sm')[DRIVER_AND_PASSENGER];
             complectCard.dispatchEvent(new Event('click'));
             document.body.style.overflow = 'scroll';
             setTimeout(initComplectOptions, 50, goodOptions);
             break;
         case 'Комплект на весь салон':
-            сonsole.log('Комплект на вест салон');
             complectCard = document.getElementsByClassName('js-store-prod-btn t-store__card__btn t-btn t-btn_sm')[ALL_SALON];
             complectCard.dispatchEvent(new Event('click'));
             document.body.style.overflow = 'scroll';
@@ -792,14 +787,12 @@ function addToBin() {
             setTimeout(initComplectOptions, 50, goodOptions);
             break;
         case 'Комплект на весь салон в три ряда + в багажник':
-            сonsole.log('Комплект на весь салон в три ряда + в багажник');
             complectCard = document.getElementsByClassName('js-store-prod-btn t-store__card__btn t-btn t-btn_sm')[ALL_SALON_THREE_RANGE_PLUS_BAGAGE];
             complectCard.dispatchEvent(new Event('click'));
             document.body.style.overflow = 'scroll';
             setTimeout(initComplectOptions, 50, goodOptions);
             break;
         case 'Багажник':
-            сonsole.log('Багажник');
             complectCard = document.getElementsByClassName('js-store-prod-btn t-store__card__btn t-btn t-btn_sm')[BAGAGE];
             complectCard.dispatchEvent(new Event('click'));
             document.body.style.overflow = 'scroll';
